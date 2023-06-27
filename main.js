5c23d4aeb574f13482d9f36b803ee5458676845f
Maciej Janta-Lipinski <maciekjl1993@gmail.com>
2023-06-27 21:01:29 +0200

    partially working

---
+++ main.js
@@ -0,0 +1,20 @@
+import './style.css'
+import './lib/scss/main.scss'
+import initChart from './lib/js/chart.js'
+
+document.querySelector('#app').innerHTML = `
+  <main>
+     <canvas id="chart">
+     </canvas>
+     <form id="options-form">
+     <label for="reference-currency">Reference currency</label>
+        <select name="reference-currency" id="reference-currency"></select>
+        <div id="currencies">
+        </div>
+        <button id="show-button">Show</button>
+     </form>
+  </main>
+  
+`
+
+initChart();
