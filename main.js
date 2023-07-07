09ac543c4bb72cbc7fe2e994798576af403ddc70
Maciej Janta-Lipinski <maciekjl1993@gmail.com>
2023-07-07 20:28:18 +0200

    Partially working - need to fix apiUrl and solve an issue with rates below value of 1

--- main.js
+++ main.js
@@ -11,9 +11,15 @@ document.querySelector('#app').innerHTML = `
         <select name="reference-currency" id="reference-currency">
             <option value="" disabled selected>Select...</option>
         </select>
+        <select name="timespan" id="timespan">
+            <option value="5">Last week</option>
+            <option value="12">Last 14 days</option>
+            <option value="${new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate() - 2}">Last 30 days</option>
+        </select>
+        
         <div id="currencies">
         </div>
-        <button id="show-button">Show</button>
+        <button id="show-button" disabled>Show</button>
      </form>
   </main>
 `
