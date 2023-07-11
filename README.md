db48ab4635c23f983fe125c3ce1a5a5a7d8f1f86
Maciej Janta-Lipinski <maciekjl1993@gmail.com>
2023-07-11 19:53:09 +0200

    Ready to use

---
+++ README.md
@@ -0,0 +1,10 @@
+# currency-rate-chart
+
+## Prerequisities
+1. Node.js
+2. 
+## Installation
+1. Clone this repository
+2. Run ```npm install```
+3. Run ```npm run dev```
+4. Enjoy!
--- main.js
+++ main.js
@@ -11,12 +11,13 @@ document.querySelector('#app').innerHTML = `
         <select name="reference-currency" id="reference-currency">
             <option value="" disabled selected>Select...</option>
         </select>
+        <label for="timespan">Timespan</label>
         <select name="timespan" id="timespan">
             <option value="5">Last week</option>
             <option value="12">Last 14 days</option>
             <option value="${new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate() - 2}">Last 30 days</option>
         </select>
-        
+        <label for="currencies">Currencies to show</label>
         <div id="currencies">
         </div>
         <button id="show-button" disabled>Show</button>
